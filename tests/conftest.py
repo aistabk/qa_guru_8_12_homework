00b7b7f1d03bede9af0c9defea3feb54b498e902
aistabk <elizavetasoom@ya.ru>
2023-10-28 10:26:58 +0300

    first commit

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from utils import attach
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "117.0",
+        "browser.config.window_width = 700"
+        "browser.config.window_height = 1900"
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
---
+++ tests/test_mid-level-step-objects.py
@@ -0,0 +1,40 @@
+from selene.support.shared import browser
+import allure
+from model.practice_form import PracticeForm
+
+
+@allure.title("Проверка работы формы регистрации")
+def test_register_form():
+    practice_form = PracticeForm()
+    practice_form.open()
+
+    with allure.step("Заполнение полей формы"):
+        practice_form.fill_first_name('Имя')
+        practice_form.fill_last_name('Фамилия')
+        practice_form.fill_email('testmail@mail.gg')
+        practice_form.fill_phone_number('2589632147')
+        practice_form.gender_select()
+        practice_form.fill_date_of_birth(1985, 10, 19)
+        practice_form.select_hobby()
+        practice_form.fill_subject('P')
+    with allure.step("Добавление фотографии"):
+        practice_form.add_photo('test_image/hedgehog.jpg')
+    with allure.step("Заполнение адреса"):
+        practice_form.fill_address('221b', 'Baker Street', 'London', 'NW1 6XE', 'UK')
+        practice_form.state_select('NCR').city_select('Delhi')
+    with allure.step("Подтверждение регистрации пользователя"):
+        browser.element('#submit').click()
+
+    with allure.step("Проверка введенных данных"):
+        practice_form.should_registrated_user_with(
+            'Имя Фамилия',
+            'testmail@mail.gg',
+            'Other',
+            '2589632147',
+            '19 November,1985',
+            'Physics',
+            'Reading',
+            'hedgehog.jpg',
+            '221b, Baker Street, London, NW1 6XE, UK',
+            'NCR Delhi'
+        )
